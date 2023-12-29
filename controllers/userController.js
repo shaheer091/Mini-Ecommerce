5252d7e4bb3fcae9b4f53a1eebd7dd4d9a531115
shaheer091 <shaheerbinmuhammed6@gmail.com>
2023-12-29 14:12:34 +0530

    completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,10 @@
 const users = require("../models/user");
 const bcrypt = require("bcrypt");
 const productList = require('../models/products')
+const userDetailsModel = require('../models/userDetails')
+
+const { default: mongoose } = require('mongoose')
+
 
 //get request
 const signup = (req, res) => {
@@ -22,27 +26,37 @@ const login = (req, res) => {
 
 const home = (req, res) => {
     if (req.session.user) {
+        console.log('req.session.userId', req.session.userId);
         res.render("user/home");
     } else {
         res.redirect('/login')
     }
 };
 
-const profile = (req, res) => {
+const profile = async (req, res) => {
     if (req.session.user) {
-        res.render("user/profile");
+        const userId = req.session.userId
+        console.log('userId', userId)
+        // const user = await users.findOne({ email })
+        const allData = await users.aggregate([{ $match: { _id: new mongoose.Types.ObjectId(userId) } }, { $lookup: { from: "userdetails", localField: "_id", foreignField: "userID", as: "fullDetails" } }])
+        console.log(allData);
+        res.render("user/profile", { allData });
     } else {
         res.redirect('/login')
     }
 };
 
 const products = async (req, res) => {
-    try {
-        const productDetails = await productList.find()
-        console.log(productDetails);
-        res.render("user/products", { products: productDetails })
-    } catch (error) {
-        console.log(error);
+    if (req.session.user) {
+        try {
+            const productDetails = await productList.find()
+            console.log(productDetails);
+            res.render("user/products", { products: productDetails })
+        } catch (error) {
+            console.log(error);
+        }
+    } else {
+        res.redirect('/login')
     }
 };
 
@@ -51,14 +65,16 @@ const createUser = async (req, res) => {
     try {
         const { username, email, password } = req.body;
         if (!username || !email || !password) {
-            res.redirect("signup");
+            console.log('first');
+            res.redirect("/signup");
         }
         const existingUser = await users.findOne({
             $or: [{ username }, { email }],
         });
 
         if (existingUser) {
-            res.redirect('login?alert=User already exist')
+            console.log('second');
+            res.redirect('/login?alert=User already exist')
         } else {
             const hashedPassword = await bcrypt.hash(password, 10);
 
@@ -67,8 +83,11 @@ const createUser = async (req, res) => {
                 email,
                 password: hashedPassword,
             });
+            req.session.userId = newUser._id;
+            req.session.user = 'shaheer'
+            console.log(newUser);
+            console.log(req.session.userId);
             await newUser.save();
-            req.session.user = email;
             res.redirect("/home");
         }
 
@@ -99,12 +118,12 @@ const loginedUser = async (req, res) => {
             return res.redirect("/login");
         }
         req.session.user = email;
-        console.log(existingUser)
         if (existingUser.userType == "admin") {
             req.session.isAdmin = true;
             res.redirect("/adminHome");
         } else {
             req.session.isAdmin = false;
+            req.session.userId = existingUser._id
             res.redirect("/home");
         }
 
@@ -112,6 +131,34 @@ const loginedUser = async (req, res) => {
         console.log(error);
     }
 };
+
+const addDetails = async (req, res) => {
+    try {
+        const used = req.session.userId
+        const { username, email, fullname, age, gender, phonenumber, address } = req.body
+        console.log(username);
+        console.log("------------------------------------------");
+        console.log(req.body);
+        console.log("------------------------------------------");
+
+
+        console.log('used----', used);
+        await userDetailsModel.updateOne(
+            { userID: used },
+            {
+                fullName: fullname,
+                gender: gender,
+                age: age,
+                phoneNumber: phonenumber,
+                address: address,
+                userID: used
+            },
+            { upsert: true })
+        res.redirect('/home')
+    } catch (err) {
+        console.log(err);
+    }
+}
 
 const logout = (req, res) => {
     try {
@@ -127,14 +174,6 @@ const logout = (req, res) => {
     }
 };
 
-// const updateDetails = async (req, res) => {
-//     try {
-//         const { fullname, gender, place } = req.body
-
-//     }
-// }
-
-
 
 module.exports = {
     signup,
@@ -145,5 +184,5 @@ module.exports = {
     createUser,
     loginedUser,
     logout,
-
+    addDetails
 };
--- models/userDetails.js
+++ models/userDetails.js
@@ -1,25 +1,30 @@
 const mongoose = require("mongoose");
 const userDetails = new mongoose.Schema({
-    fullname: {
+    fullName: {
         type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
+        required: true
     },
     gender: {
         type: String,
-        required: true,
+        required: true
     },
-    place: {
+    age: {
+        type: Number,
+        required: true
+    },
+    phoneNumber: {
+        type: Number,
+        required: true
+    },
+    address: {
         type: String,
-        default: "user",
+        required: true
     },
+    userID: {
+        type: mongoose.Types.ObjectId,
+        required: true
+    }
 });
 const userDetailsModel = mongoose.model("UserDetails", userDetails);
 
-
-
-
-module.exports = userDetails;
+module.exports = userDetailsModel;
